4cb373fa9a981c45d91ab44d875ebf44a07347e2
IcaroTimoteo96 <timoteoicaro18@gmail.com>
2024-11-09 17:45:53 -0300

    Adição de bibliotecas

--- Cart-Challenge/Program.cs
+++ Cart-Challenge/Program.cs
@@ -1,3 +1,9 @@
+using Cart.Application.CasosDeUso;
+using Cart.Domain.Interfaces;
+using Cart.Infraestrutura.Repositorios;
+using Cart.Infraestrutura;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<AdicionarItemCarrinho>();
+builder.Services.AddScoped<BuscarItensCarrinho>();
+builder.Services.AddScoped<RemoverItemCarrinho>();
+
+builder.Services.AddScoped<ICarrinhoRepository, CarrinhoRepository>();
+builder.Services.AddScoped<IItemCarrinhoRepository, ItemCarrinhoRepository>();
 
 var app = builder.Build();
 
